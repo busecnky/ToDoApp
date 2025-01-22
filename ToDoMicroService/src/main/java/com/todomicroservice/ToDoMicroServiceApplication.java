0805d6dcc233ba4df5c55c2b2a8170faa38c5b57
busecnky <cankayabuse94@gmail.com>
2025-01-22 23:45:30 +0300

    chore: add microservices

---
+++ ToDoMicroService/src/main/java/com/todomicroservice/ToDoMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.todomicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoMicroServiceApplication.class, args);
+	}
+
+}
---
+++ ToDoMicroService/src/test/java/com/todomicroservice/ToDoMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todomicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
