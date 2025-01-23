d7396985713d5934fe706c72e346e5c19306cac9
busecnky <cankayabuse94@gmail.com>
2025-01-24 01:08:04 +0300

    chore: add classes and packages

--- ToDoMicroService/src/main/java/com/todomicroservice/repository/ToDoRepository.java
+++ ToDoMicroService/src/main/java/com/todomicroservice/repository/ToDoRepository.java
@@ -1,6 +1,7 @@
 package com.todomicroservice.repository;
 
 import com.todomicroservice.entity.ToDo;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ToDoRepository extends JpaRepository<ToDo, Integer> {
+public interface ToDoRepository extends JpaRepository<ToDo, Long> {
 }
