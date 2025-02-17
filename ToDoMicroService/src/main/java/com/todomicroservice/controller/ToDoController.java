0cf558cd2e5379a410590b1d7561b23bb95f6834
busecnky <cankayabuse94@gmail.com>
2025-02-17 23:57:07 +0300

    chore: add layers

--- ToDoMicroService/src/main/java/com/todomicroservice/controller/ToDoController.java
+++ ToDoMicroService/src/main/java/com/todomicroservice/controller/ToDoController.java
@@ -1,11 +1,38 @@
 package com.todomicroservice.controller;
 
+import com.todomicroservice.dto.request.ToDoRequestDto;
+import com.todomicroservice.dto.response.ToDoResponseDto;
+import com.todomicroservice.entity.ToDo;
+import com.todomicroservice.service.ToDoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
-@RequiredArgsConstructor
 public class ToDoController {
+    private final ToDoService toDoService;
+
+    public ToDoController(ToDoService toDoService) {
+        this.toDoService = toDoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ToDo>> getAllTodos() {
+        return ResponseEntity.ok(toDoService.getAllToDos());
+    }
+
+    @PostMapping
+    public ResponseEntity<ToDo> createTodo(@RequestBody ToDo todo) {
+        return ResponseEntity.ok(toDoService.createToDo(todo));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable Long id) {
+        toDoService.deleteToDo(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- ToDoMicroService/src/main/java/com/todomicroservice/service/ToDoService.java
+++ ToDoMicroService/src/main/java/com/todomicroservice/service/ToDoService.java
@@ -1,7 +1,30 @@
 package com.todomicroservice.service;
 
+import com.todomicroservice.dto.request.ToDoRequestDto;
+import com.todomicroservice.dto.response.ToDoResponseDto;
+import com.todomicroservice.entity.ToDo;
+import com.todomicroservice.repository.ToDoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ToDoService {
+    private final ToDoRepository toDoRepository;
+
+    public ToDoService(ToDoRepository toDoRepository) {
+        this.toDoRepository = toDoRepository;
+    }
+
+    public List<ToDo> getAllToDos() {
+        return toDoRepository.findAll();
+    }
+
+    public ToDo createToDo(ToDo todo) {
+        return toDoRepository.save(todo);
+    }
+
+    public void deleteToDo(Long id) {
+        toDoRepository.deleteById(id);
+    }
 }
