0cc78bae1ce2aaf5b41a741552b7bc3f5cbb811e
busecnky <cankayabuse94@gmail.com>
2025-02-21 23:58:13 +0300

    chore: add ApiGateway

---
+++ ApiGatewayMicroService/src/main/java/com/apigatewaymicroservice/ApiGatewayMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apigatewaymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayMicroServiceApplication.class, args);
+    }
+
+}
